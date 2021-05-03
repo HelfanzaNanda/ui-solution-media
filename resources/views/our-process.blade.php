4a9858a50b5222e2096f0e45881f5b1ea153c9e0
HelfanzaNanda <elvannnd@gmail.com>
2021-05-03 20:43:42 +0700

    z-index

--- resources/views/our-process.blade.php
+++ resources/views/our-process.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('content')
 <div class="flex flex-col h-screen">
-    <div class="fixed px-10 top-0 left-0">
+    <div class="fixed px-10 top-0 left-0 z-20">
         <div>
             <img src="{{ asset('assets/images/logo.png') }}" alt="">
         </div>
     </div>
-    <div class="fixed px-20 top-0 right-0 py-10">
+    <div class="fixed px-20 top-0 right-0 py-10 z-20">
         <div>
             <img src="{{ asset('assets/images/toggle.png') }}" alt="" class="h-12 w-12">
         </div>
