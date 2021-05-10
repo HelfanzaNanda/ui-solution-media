71faa9554122fbb1a0c54937bac77fb4af6edeb3
Damar Permadany <damarp2017@gmail.com>
2021-05-10 12:45:05 +0700

    desktop-50

--- resources/views/desktop-50.blade.php
+++ resources/views/desktop-50.blade.php
@@ -1,13 +1,7 @@
 @extends('layouts.app')
 @push('styles')
 <style>
-    .gradient--text {
-        background-image: linear-gradient(to right, rgb(128, 0, 107), rgb(208, 15, 176), white, white, white, white, yellow);
-        /* Standard syntax; must be last */
-        color: transparent;
-        -webkit-background-clip: text;
-        background-clip: text;
-    }
+
 </style>
 @endpush
 @section('content')
@@ -20,15 +14,325 @@
         <div> <a href=""> <img src="{{ asset('assets/images/toggle.png') }}" alt="" class="h-12 w-12"> </a> </div>
     </div>
 
-    <main class="flex-1 overflow-y-auto bg-gray-50 overflow-x-hidden">
+    <main class="flex-1 overflow-y-auto bg-gray-50 overflow-x-auto overflow-x-hidden">
         <section class="bg--dark p-20">
-            <h1 class="text-white gradient--text font--gilroy-bold" style="font-size: 100px;">
-                Case Study
-            </h1>
-            <h5 class="text-white text-opacity-40 text-xs my-5" style="font-size: 18px;">
-                for world-class brands to make your products to live for world-class brands to make your products to
-                live for world-class brands to make your products to
-            </h5>
+            <div class="flex flex-col ml-52">
+                <div class="flex items-center space-x-2">
+                    <h4 class="text-white font--gilroy-bold opacity-50" style="font-size: 50px;">01</h4>
+                    <hr class="w-80" style="border: 1px solid #5C5C5C">
+                </div>
+                <h1 class="text-white font--gilroy-bold -mr-36 leading-none" style="font-size: 220px">Case Study</h1>
+                <div class="flex">
+                    <div class="w-1/2">
+                        <p class="text-white opacity-60">
+                            for world-class brands to make your products to live for world-class brands to make your pro
+                            .
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-end">
+                <h1 class="text-white font--gilroy-bold opacity-30 leading-none" style="font-size: 400px;">01</h1>
+            </div>
+            <div class="flex justify-center">
+                <a href="" class="-mt-20">
+                    <img src="{{ asset('assets/images/scroll.png') }}" alt="">
+                </a>
+            </div>
+        </section>
+        <section class="p-20"
+            style="background-image: url({{ asset('assets/images/bg.png') }}); height:140%; background-position: center; background-repeat: no-repeat; background-size: cover;">
+            <div class="flex flex-col mt-20 space-y-10">
+                <img src="{{ asset('assets/images/search.png') }}" alt="" class="mx-auto">
+                <h1 class="text-white font--gilroy-bold leading-none text-center" style="font-size: 90px">Case Study
+                </h1>
+                <h4 class="text-white text-center leading-8" style="font-size: 19px">
+                    for world-class brands to make your products to live for world-class brands to make your products to
+                    live for world-class brands to make your products to for world-class brands to make your products to
+                    live for world-class brands to for world-class brands to make your products to live for world-class
+                    brands to make your products to live for world-class brands to make your products to for world-class
+                    brands to make your products to live for world-class brands to
+                </h4>
+            </div>
+            <div class="flex justify-center space-x-1 mt-60">
+                <div class="w-1/3">
+
+                </div>
+                <div class="w-1/3 flex justify-center">
+                    <a href="" class="-mb-10">
+                        <img src="{{ asset('assets/images/scroll.png') }}" alt="">
+                    </a>
+                </div>
+                <div class="w-1/3 flex space-x-2">
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white w-1/5">
+                        <i class="fa fa-city text-white text-2xl mx-auto self-end"></i>
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="bg--dark p-20">
+            <div class="flex flex-col ml-52">
+                <div class="flex items-center space-x-2">
+                    <h4 class="text-white font--gilroy-bold opacity-50" style="font-size: 50px;">02</h4>
+                    <hr class="w-80" style="border: 1px solid #5C5C5C">
+                </div>
+                <h1 class="text-white font--gilroy-bold -mr-36 leading-none" style="font-size: 220px">Planning</h1>
+                <div class="flex">
+                    <div class="w-1/2">
+                        <p class="text-white opacity-60">
+                            for world-class brands to make your products to live for world-class brands to make your pro
+                            .
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-end">
+                <h1 class="text-white font--gilroy-bold opacity-30 leading-none" style="font-size: 400px;">02</h1>
+            </div>
+            <div class="flex justify-center">
+                <a href="" class="-mt-20">
+                    <img src="{{ asset('assets/images/scroll.png') }}" alt="">
+                </a>
+            </div>
+        </section>
+        <section class="p-20"
+            style="background-image: url({{ asset('assets/images/bg.png') }}); height:140%; background-position: center; background-repeat: no-repeat; background-size: cover;">
+            <div class="flex flex-col mt-20 space-y-10">
+                <img src="{{ asset('assets/images/calendar.png') }}" alt="" class="mx-auto">
+                <h1 class="text-white font--gilroy-bold leading-none text-center" style="font-size: 90px">
+                    Planning
+                </h1>
+                <h4 class="text-white text-center leading-8" style="font-size: 19px">
+                    for world-class brands to make your products to live for world-class brands to make your products to
+                    live for world-class brands to make your products to for world-class brands to make your products to
+                    live for world-class brands to for world-class brands to make your products to live for world-class
+                    brands to make your products to live for world-class brands to make your products to for world-class
+                    brands to make your products to live for world-class brands to
+                </h4>
+            </div>
+            <div class="flex justify-center space-x-1 mt-60">
+                <div class="w-1/3">
+
+                </div>
+                <div class="w-1/3 flex justify-center">
+                    <a href="" class="-mb-10">
+                        <img src="{{ asset('assets/images/scroll.png') }}" alt="">
+                    </a>
+                </div>
+                <div class="w-1/3 flex space-x-2">
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white w-1/5">
+                        <i class="fa fa-city text-white text-2xl mx-auto self-end"></i>
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="bg--dark p-20">
+            <div class="flex flex-col ml-52">
+                <div class="flex items-center space-x-2">
+                    <h4 class="text-white font--gilroy-bold opacity-50" style="font-size: 50px;">03</h4>
+                    <hr class="w-80" style="border: 1px solid #5C5C5C">
+                </div>
+                <h1 class="text-white font--gilroy-bold -mr-36 leading-none" style="font-size: 220px">Research</h1>
+                <div class="flex">
+                    <div class="w-1/2">
+                        <p class="text-white opacity-60">
+                            for world-class brands to make your products to live for world-class brands to make your pro
+                            .
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-end">
+                <h1 class="text-white font--gilroy-bold opacity-30 leading-none" style="font-size: 400px;">03</h1>
+            </div>
+            <div class="flex justify-center">
+                <a href="" class="-mt-20">
+                    <img src="{{ asset('assets/images/scroll.png') }}" alt="">
+                </a>
+            </div>
+        </section>
+        <section class="p-20"
+            style="background-image: url({{ asset('assets/images/bg.png') }}); height:140%; background-position: center; background-repeat: no-repeat; background-size: cover;">
+            <div class="flex flex-col mt-20 space-y-10">
+                <img src="{{ asset('assets/images/empty-files.png') }}" alt="" class="mx-auto">
+                <h1 class="text-white font--gilroy-bold leading-none text-center" style="font-size: 90px">
+                    Research
+                </h1>
+                <h4 class="text-white text-center leading-8" style="font-size: 19px">
+                    for world-class brands to make your products to live for world-class brands to make your products to
+                    live for world-class brands to make your products to for world-class brands to make your products to
+                    live for world-class brands to for world-class brands to make your products to live for world-class
+                    brands to make your products to live for world-class brands to make your products to for world-class
+                    brands to make your products to live for world-class brands to
+                </h4>
+            </div>
+            <div class="flex justify-center space-x-1 mt-60">
+                <div class="w-1/3">
+
+                </div>
+                <div class="w-1/3 flex justify-center">
+                    <a href="" class="-mb-10">
+                        <img src="{{ asset('assets/images/scroll.png') }}" alt="">
+                    </a>
+                </div>
+                <div class="w-1/3 flex space-x-2">
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white w-1/5">
+                        <i class="fa fa-city text-white text-2xl mx-auto self-end"></i>
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="bg--dark p-20">
+            <div class="flex flex-col ml-52">
+                <div class="flex items-center space-x-2">
+                    <h4 class="text-white font--gilroy-bold opacity-50" style="font-size: 50px;">04</h4>
+                    <hr class="w-80" style="border: 1px solid #5C5C5C">
+                </div>
+                <h1 class="text-white font--gilroy-bold -mr-36 leading-none" style="font-size: 220px">Development</h1>
+                <div class="flex">
+                    <div class="w-1/2">
+                        <p class="text-white opacity-60">
+                            for world-class brands to make your products to live for world-class brands to make your pro
+                            .
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-end">
+                <h1 class="text-white font--gilroy-bold opacity-30 leading-none" style="font-size: 400px;">04</h1>
+            </div>
+            <div class="flex justify-center">
+                <a href="" class="-mt-20">
+                    <img src="{{ asset('assets/images/scroll.png') }}" alt="">
+                </a>
+            </div>
+        </section>
+        <section class="p-20"
+            style="background-image: url({{ asset('assets/images/bg.png') }}); height:140%; background-position: center; background-repeat: no-repeat; background-size: cover;">
+            <div class="flex flex-col mt-20 space-y-10">
+                <img src="{{ asset('assets/images/computer.png') }}" alt="" class="mx-auto">
+                <h1 class="text-white font--gilroy-bold leading-none text-center" style="font-size: 90px">
+                    Development
+                </h1>
+                <h4 class="text-white text-center leading-8" style="font-size: 19px">
+                    for world-class brands to make your products to live for world-class brands to make your products to
+                    live for world-class brands to make your products to for world-class brands to make your products to
+                    live for world-class brands to for world-class brands to make your products to live for world-class
+                    brands to make your products to live for world-class brands to make your products to for world-class
+                    brands to make your products to live for world-class brands to
+                </h4>
+            </div>
+            <div class="flex justify-center space-x-1 mt-60">
+                <div class="w-1/3">
+
+                </div>
+                <div class="w-1/3 flex justify-center">
+                    <a href="" class="-mb-10">
+                        <img src="{{ asset('assets/images/scroll.png') }}" alt="">
+                    </a>
+                </div>
+                <div class="w-1/3 flex space-x-2">
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white w-1/5">
+                        <i class="fa fa-city text-white text-2xl mx-auto self-end"></i>
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="bg--dark p-20">
+            <div class="flex flex-col ml-52">
+                <div class="flex items-center space-x-2">
+                    <h4 class="text-white font--gilroy-bold opacity-50" style="font-size: 50px;">05</h4>
+                    <hr class="w-80" style="border: 1px solid #5C5C5C">
+                </div>
+                <h1 class="text-white font--gilroy-bold -mr-36 leading-none" style="font-size: 220px">Deployment</h1>
+                <div class="flex">
+                    <div class="w-1/2">
+                        <p class="text-white opacity-60">
+                            for world-class brands to make your products to live for world-class brands to make your pro
+                            .
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-end">
+                <h1 class="text-white font--gilroy-bold opacity-30 leading-none" style="font-size: 400px;">05</h1>
+            </div>
+            <div class="flex justify-center">
+                <a href="" class="-mt-20">
+                    <img src="{{ asset('assets/images/scroll.png') }}" alt="">
+                </a>
+            </div>
+        </section>
+        <section class="p-20"
+            style="background-image: url({{ asset('assets/images/bg.png') }}); height:140%; background-position: center; background-repeat: no-repeat; background-size: cover;">
+            <div class="flex flex-col mt-20 space-y-10">
+                <img src="{{ asset('assets/images/succes.png') }}" alt="" class="mx-auto">
+                <h1 class="text-white font--gilroy-bold leading-none text-center" style="font-size: 90px">
+                    Deployment
+                </h1>
+                <h4 class="text-white text-center leading-8" style="font-size: 19px">
+                    for world-class brands to make your products to live for world-class brands to make your products to
+                    live for world-class brands to make your products to for world-class brands to make your products to
+                    live for world-class brands to for world-class brands to make your products to live for world-class
+                    brands to make your products to live for world-class brands to make your products to for world-class
+                    brands to make your products to live for world-class brands to
+                </h4>
+            </div>
+            <div class="flex justify-center space-x-1 mt-60">
+                <div class="w-1/3">
+
+                </div>
+                <div class="w-1/3 flex justify-center">
+                    <a href="" class="-mb-10">
+                        <img src="{{ asset('assets/images/scroll.png') }}" alt="">
+                    </a>
+                </div>
+                <div class="w-1/3 flex space-x-2">
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white w-1/5">
+                        <i class="fa fa-city text-white text-2xl mx-auto self-end"></i>
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                    <div class="flex py-5 border-b-2 border-white opacity-20 w-1/5">
+                    </div>
+                </div>
+            </div>
         </section>
         <x-footer />
     </main>
@@ -36,4 +340,7 @@
 @endsection
 
 @push('scripts')
+<script>
+
+</script>
 @endpush
--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/our-process', 'our-process');
-Route::view('/case-studies', 'case-studies');
+// Route::view('/menu', 'menu');
+// Route::view('/desktop-14', 'desktop-14');
+Route::view('/see-more', 'see-more');
 Route::view('/philosophy', 'philosophy');
+Route::view('/case-studies', 'case-studies');
+Route::view('/desktop-50', 'desktop-50');
+Route::view('/capabilities', 'capabilities');
+Route::view('/careers', 'careers');
+// Route::view('/blogs', 'blogs');
+// Route::view('/insight', 'insight');
 Route::view('/contact-us', 'contact-us');
 Route::view('/privacy-policy', 'privacy-policy');
 Route::view('/terms-and-conditions', 'terms-and-conditions');
 Route::view('/opportunities', 'opportunities');
-Route::view('/capabilities', 'capabilities');
-Route::view('/careers', 'careers');
-Route::view('/desktop-50', 'desktop-50');
+// Route::view('/blog-detail', 'blog-detail');
+// Route::view('/terms-and-conditions-2', 'terms-and-conditions-2');
 Route::view('/test', 'try-conic-gradient');
-Route::view('/see-more', 'see-more');
